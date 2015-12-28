7e1f3629e940dc6f8262e9d68c49c09674053de6
jakob arndal <takana@exelion.dk>
2015-12-28 12:41:35 +0100

    Første commit af denne threadmulti chat ting!

---
+++ Server/Server.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServerData;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Net;
+
+
+namespace Server
+{
+    class Server
+    {
+        static Socket listenerSocket;
+        static List<ClientData> _client;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("staring server on " + Packet.GetIP4Address());
+            listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _client = new List<ClientData>();
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(Packet.GetIP4Address()),4242);
+            listenerSocket.Bind(ip);
+            Thread listenThread = new Thread(ListenThread);
+            listenThread.Start();
+        }
+
+        public static void Data_IN(object cSocket) {
+            Socket clientSocket = (Socket)cSocket;
+            byte[] Buffer;
+            int readBytes;
+            for (; ;)
+            {
+                try
+                {
+                    Buffer = new byte[clientSocket.SendBufferSize];
+                    readBytes = clientSocket.Receive(Buffer);
+                    if (readBytes > 0)
+                    {
+                        Packet packet = new Packet(Buffer);
+                        DataManager(packet);
+                    }
+                }
+                catch (SocketException ex) {
+                    //Console.WriteLine(ex);
+                    Console.WriteLine("A Client Disconnected!");
+                }
+            }
+
+        }
+
+        public static void DataManager(Packet p) {
+            switch (p.PacketType)
+            {
+                case PacketType.Registration:
+                    break;
+                case PacketType.Chat:
+                    foreach (ClientData c in _client)
+                    {
+                        c.clientSocket.Send(p.ToBytes());
+                    }
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+        static void ListenThread()
+        {
+
+            for (;;)
+            {
+
+
+                listenerSocket.Listen(0);
+                _client.Add(new ClientData(listenerSocket.Accept()));
+            }
+        }
+
+    }
+    class ClientData
+    {
+        public Socket clientSocket;
+        public Thread clientThread;
+  
+        public string id;
+
+        public ClientData()
+        {
+            id = Guid.NewGuid().ToString();
+            clientThread = new Thread(Server.Data_IN);
+            clientThread.Start(clientSocket);
+            SendRegistrationPacket();
+        }
+
+        public ClientData(Socket clientSocket)
+        {
+            this.clientSocket = clientSocket;
+            id = Guid.NewGuid().ToString();
+            clientThread = new Thread(Server.Data_IN);
+            clientThread.Start(clientSocket);
+            SendRegistrationPacket();
+
+        }
+
+        public void SendRegistrationPacket() {
+            Packet p = new Packet(PacketType.Registration, "server");
+            p.Gdata.Add(id);
+            clientSocket.Send(p.ToBytes());
+        }
+    }
+}
---
+++ ServerData/Packet.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Net;
+
+namespace ServerData
+{
+    [Serializable]
+    public class Packet
+    {
+        public List<string> Gdata;
+        public int packetInt;
+        public bool packetBool;
+        public string senderID;
+        public PacketType PacketType;
+
+        public Packet(PacketType type, string senderID) {
+            Gdata = new List<string>();
+            this.senderID = senderID;
+            this.PacketType = type;
+        }
+
+        public Packet(byte[] packetbytes) {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream(packetbytes);
+            Packet p = (Packet)bf.Deserialize(ms);
+            ms.Close();
+            this.Gdata = p.Gdata;
+            this.packetInt = p.packetInt;
+            this.packetBool = p.packetBool;
+            this.senderID = p.senderID;
+            this.PacketType = p.PacketType;
+
+
+
+        }
+
+        public byte[] ToBytes() {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, this);
+            byte[] bytes = ms.ToArray();
+            ms.Close();
+            return bytes;
+
+
+
+        }
+
+        public static string GetIP4Address()
+        {
+            IPAddress[] ips = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress item in ips)
+            {
+                if (item.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork )
+                {
+                    return item.ToString();
+                }
+                
+            }
+            return "127.0.0.1";
+        }
+
+    }
+
+    public enum PacketType {
+        Registration,
+        Chat
+    }
+}
